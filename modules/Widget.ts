9b15a2906be11bfa71cff80c26850ec4a86db0c8
Tandhjulet <madsbechmortensen@hotmail.dk>
2024-06-15 00:42:19 +0200

    fixed an issue with private appoinemnts; changed a bit of the values in the widget

--- modules/Widget.ts
+++ modules/Widget.ts
@@ -1,9 +1,20 @@
 import { Platform } from "react-native";
 import SharedGroupPreferences from "react-native-shared-group-preferences";
 import { Day } from "./api/scraper/SkemaScraper";
-import { formatDate } from "../pages/Skema";
 import { getDay } from "./Date";
 import Constants from "expo-constants"
+
+export function formatDate(dateString: string): Date {
+    const padded = dateString.padStart(4, "0");
+    const minutes = padded.slice(2);
+    const hours = padded.slice(0,2);
+
+    const date = new Date();
+    date.setMinutes(parseInt(minutes));
+    date.setHours(parseInt(hours));
+
+    return date;
+}
 
 enum Status {
     changed,
@@ -48,7 +59,7 @@ export async function saveCurrentSkema(day: Day[]) {
     const parsePercents = (p: string) => parseInt(p.replace("%", "").trim())/100
 
     const out: WidgetData = day.slice(i).reduce((a, v) => {
-        const copy = now;
+        const currDate = now.getDate();
 
         const out: EncodedModul[] = []
         v.moduler.forEach((modul) => {
@@ -62,9 +73,9 @@ export async function saveCurrentSkema(day: Day[]) {
                 title: modul.title ?? modul.team.join(", ")
             })
         });
-        now.setDate(now.getDate() + 1)
 
-        return { ...a, [getDay(copy).dayNumber]: out}
+        now.setDate(currDate + 1)
+        return { ...a, [currDate]: out}
     }, {}) // no reason to store from previous days
     console.log(out);
 
