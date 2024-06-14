1300c6058703199b3a304e38d22ea2a86c05e39d
Mads <MadsMortensen@Macbook-Pro.local>
2024-06-14 21:42:48 +0200

    added bacon as target manager

--- plugin/build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const withWidgetIos_1 = require("./ios/withWidgetIos");
-const withAppConfigs = (config, options) => {
-    //config = withWidgetAndroid(config)
-    config = (0, withWidgetIos_1.withWidgetIos)(config, options);
-    return config;
-};
-exports.default = withAppConfigs;
