1a98c43879d7cc95fa49b57cc41e5299b71a19ee
Mads <MadsMortensen@Macbook-Pro.local>
2024-06-14 20:31:16 +0200

    updated widget

--- widget/ios/widget/widget.swift
+++ widget/ios/widget/widget.swift
@@ -5,13 +5,14 @@
 
 import WidgetKit
 import SwiftUI
+import Intents
 
 struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
         var lookAhead: [Date] = []
         let entryDate = Date()
       
-        for hourOffset in 0 ..< 5 {
+        for hourOffset in 0 ..< 3 {
           lookAhead.append(Calendar.current.date(byAdding: .hour, value: hourOffset, to: entryDate)!)
         }
             
@@ -25,7 +26,7 @@ struct Provider: TimelineProvider {
         var lookAhead: [Date] = []
         let entryDate = Date()
       
-        for hourOffset in 0 ..< 5 {
+        for hourOffset in 0 ..< 3 {
           lookAhead.append(Calendar.current.date(byAdding: .hour, value: hourOffset, to: entryDate)!)
         }
       
@@ -36,25 +37,53 @@ struct Provider: TimelineProvider {
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-
-        let currentDate = Date()
-        for timeLineHourOffset in 0 ..< 5 {
-            var lookAhead: [Date] = []
-            let entryDate = Calendar.current.date(byAdding: .hour, value: timeLineHourOffset, to: currentDate)!
-          
-            for hourOffset in 0 ..< 5 {
-              lookAhead.append(Calendar.current.date(byAdding: .hour, value: hourOffset, to: entryDate)!)
+        let userDefaults = UserDefaults.init(suiteName: "group.widget");
+        if(userDefaults != nil) {
+            let entryDate = Date()
+            if let savedData = userDefaults!.value(forKey: "skema") as? String {
+                let decoder = JSONDecoder();
+                let data = savedData.data(using: .utf8);
+                if let parsedData = try? decoder.decode([String: [Module]].self, from: data!) {
+                    
+                    let currentDate = Calendar.current.component(.day, from: entryDate)
+                    let currentHour = Calendar.current.component(.hour, from: entryDate)
+                    let requestAfter = Calendar.current.date(byAdding: .hour, value: currentHour > 7 && currentHour < 16 ? 1 : 6, to: entryDate)!
+                    
+                    var lookAhead: [Date] = []
+                    for hourOffset in 0 ..< 3 {
+                      lookAhead.append(Calendar.current.date(byAdding: .hour, value: hourOffset, to: entryDate)!)
+                    }
+                    
+                    let entry = SimpleEntry(date: entryDate,
+                                          lookAhead: lookAhead,
+                                            modules: parsedData[String(currentDate)] ?? [])
+                        
+                    let timeline = Timeline(entries: [entry], policy: .after(requestAfter))
+                    completion(timeline)
+                }
+            } else {
+                var lookAhead: [Date] = []
+                let entryDate = Date()
+              
+                for hourOffset in 0 ..< 3 {
+                  lookAhead.append(Calendar.current.date(byAdding: .hour, value: hourOffset, to: entryDate)!)
+                }
+              
+                let entry = SimpleEntry(date: entryDate,
+                                      lookAhead: lookAhead,
+                                      modules: [])
+                let timeline = Timeline(entries: [entry], policy: .after(lookAhead.last!))
+                completion(timeline)
             }
-          
-            let entry = SimpleEntry(date: entryDate,
-                                  lookAhead: lookAhead,
-                                  modules: [])
-            entries.append(entry)
-        }
+        } else {
+            let date = Calendar.current.date(byAdding: .hour, value: 5, to: Date())
             
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
+            let entry = SimpleEntry(date: Date(),
+                                  lookAhead: [],
+                                    modules: [], error: true)
+            let timeline = Timeline(entries: [entry], policy: .after(date!))
+            completion(timeline)
+        }
     }
 }
 
@@ -73,21 +102,23 @@ extension Date {
 
 }
 
-enum Status {
+enum Status: Decodable {
   case changed
   case cancelled
   case normal
 }
 
-struct Module {
-  var start: Date
-  var end: Date
+struct Module: Decodable {
+    let start: Date
+    let end: Date
   
-  var title: String
-  var status: Status
-  var overlaps: Int = 2
+    let title: String
+    let status: Status
   
-  var _id: String
+    let _id: String
+  
+    let width: CGFloat
+    let left: CGFloat
 }
 
 struct SimpleEntry: TimelineEntry {
@@ -95,6 +126,8 @@ struct SimpleEntry: TimelineEntry {
   
     let lookAhead: Array<Date>
     let modules: Array<Module>
+    
+    var error: Bool = false
 }
 
 struct widgetEntryView : View {
@@ -102,20 +135,10 @@ struct widgetEntryView : View {
   
     func calculateTop(module: Module, first: Date, geoHeight: CGFloat) -> CGFloat {
       let diff = module.start - first;
-      var top = (diff/(60*60*5)) * geoHeight;
-      top = top < 0 ? 0 : top+4;
-      top += diff < 0 ? 0 : 4;
-
+      var top = ((diff+(2))/(60*60*2)) * (geoHeight);
+      top = top < 0 ? 0 : top;
+      
       return top;
-    }
-  
-    func calculateHeight(start: Date, end: Date, geoHeight: CGFloat) -> CGFloat {
-        var diff = end - start
-        if(start < entry.lookAhead.first!) {
-            diff -= (entry.lookAhead.first! - start)
-        }
-        
-        return (diff/(60*60*5)) * geoHeight
     }
   
     private var ItemSeperator: some View {
@@ -127,7 +150,7 @@ struct widgetEntryView : View {
               maxHeight: 0
             )
           
-          Divider().frame(height: 10)
+          Divider().frame(maxWidth: .infinity, maxHeight: 10)
              
           if entry.lookAhead.last.self != lookAhead.self {
             Spacer()
@@ -138,7 +161,6 @@ struct widgetEntryView : View {
     
     var body: some View {
       if #available(iOSApplicationExtension 17.0, *) {
-        
         VStack {
           HStack(spacing: 0) {
             Text(entry.lookAhead.first!.dayOfWeek()!)
@@ -152,76 +174,70 @@ struct widgetEntryView : View {
               .fontWeight(.regular)
           }
           
-          GeometryReader { geo in
-            HStack(spacing: 5) {
-              ZStack(alignment: .leading) {
-                ItemSeperator
-                
-                ForEach(entry.modules, id: \._id) { module in
-                  if(module.end > entry.lookAhead.first!) {
-                    let top = calculateTop(module: module, first: entry.lookAhead.first!, geoHeight: geo.size.height);
-                    
-                    VStack(spacing: 0) {
-                      Color.clear
-                        .opacity(0)
-                        .frame(
-                          maxWidth: .infinity,
-                          maxHeight: top
-                        )
+          HStack(spacing: 5) {
+            if(!entry.error) {
+              GeometryReader { geo in
+                ZStack(alignment: .leading) {
+                  ItemSeperator.frame(width: geo.size.width)
+                  
+                  ForEach(entry.modules, id: \._id) { module in
+                    if(module.end > entry.lookAhead.first!) {
+                      let top = calculateTop(module: module, first: entry.lookAhead.first!, geoHeight: geo.size.height);
                       
-                      let height = calculateHeight(start: module.start, end: module.end, geoHeight: geo.size.height) + (module.start < entry.lookAhead.first! ? 8+5 : 4)
-                      
-                      ZStack(alignment: .topLeading) {
-                          Rectangle()
-                              .fill(Color("Primary"))
+                      VStack(spacing: 0) {
+                        Color.clear
+                          .opacity(0)
+                          .frame(
+                            maxWidth: .infinity,
+                            maxHeight: top
+                          )
+                        
+                        HStack(spacing: 0) {
+                          Color.clear
+                            .frame(width: module.left * geo.size.width, height: 0)
+                          
+                          ZStack(alignment: .topLeading) {
+                            Rectangle()
+                              .fill(module.status == .normal ? Color("Primary") : module.status == .changed ? Color.yellow : Color("Red"))
                               .opacity(1)
                               .clipShape(
                                 .rect(
-                                    topLeadingRadius: module.start > entry.lookAhead.first! ? 3 : 0,
+                                  topLeadingRadius: module.start > entry.lookAhead.first! ? 3 : 0,
                                   bottomLeadingRadius: 3,
                                   bottomTrailingRadius: 3,
-                                    topTrailingRadius: module.start > entry.lookAhead.first! ? 3 : 0,
+                                  topTrailingRadius: module.start > entry.lookAhead.first! ? 3 : 0,
                                   style: .continuous
                                 )
                               )
                               .frame(
-                                height: height
+                                width: .infinity, height: .infinity
                               )
-                          
-                          if(height > 10+4*2) {
+                            
+                            if(calculateHeight(start: module.start, end: module.end) * geo.size.height > 10+4*2) {
                               ZStack {
-                                HStack(alignment: .top, spacing: 0) {
-                                  Text(module.title)
-                                    .font(.system(size: 9, weight: .bold))
-                                    .multilineTextAlignment(.leading)
-                                    .foregroundStyle(Color.white)
-                                  
-                                  if module.overlaps > 0 {
-                                    Spacer()
-                                  
-                                    Text("+" + module.overlaps.description)
-                                      .font(.system(size: 8))
-                                      .multilineTextAlignment(.leading)
-                                      .foregroundColor(Color.white)
-                                      .fontWeight(.heavy)
-                                      .opacity(0.7)
-                                  }
-                                }
-                              }.padding(4).frame(height: height)
+                                Text(module.title)
+                                  .font(.system(size: 9, weight: .bold))
+                                  .multilineTextAlignment(.leading)
+                                  .foregroundStyle(Color.white)
+                                  .minimumScaleFactor(0.6)
+                              }.padding(4)
+                            }
                           }
+                          .frame(width: module.width * geo.size.width, height: calculateHeight(start: module.start, end: module.end) * geo.size.height)
+                          
+                          Spacer(minLength: 0)
+                        }
                         
+                        Spacer(minLength: 0)
                       }
-                      .frame(width: .infinity, height: height)
-                      
-                      Spacer()
                     }
                   }
                 }
+                .frame(
+                  maxWidth: .infinity,
+                  maxHeight: .infinity
+                )
               }
-              .frame(
-                maxWidth: .infinity,
-                maxHeight: .infinity
-              )
               
               VStack(alignment: .leading, spacing: 0) {
                 ForEach(entry.lookAhead, id: \.self) { lookAhead in
@@ -241,16 +257,23 @@ struct widgetEntryView : View {
                     .multilineTextAlignment(.trailing)
                     .frame(height: 10)
                     .foregroundColor(.secondary)
-                     
+                  
                   if entry.lookAhead.last.self != lookAhead.self {
                     Spacer()
                   }
                 }
               }
+            } else {
+              
+              Text("Der opstod en fejl").frame(
+                maxWidth: .infinity,
+                maxHeight: .infinity,
+                alignment: .center
+              ).multilineTextAlignment(.center)
+              
+            }
           }
-            .containerBackground(for: .widget, content: {Color("AccentColor")})
-          }
-        }
+        }.containerBackground(for: .widget, content: {Color("AccentColor")})
         
     } else {
         Text(entry.date, style: .time)
@@ -271,12 +294,18 @@ struct widget: Widget {
     }
 }
 
+func calculateHeight(start: Date, end: Date) -> CGFloat {
+  let compareTo: Date = start < Date() ? Date() : start;
+  let diff = (end - compareTo) / (60*60*2);
+  return diff;
+}
+
 struct widget_Previews: PreviewProvider {
     static func calculateLookAhead() -> Array<Date> {
         var lookAhead: [Date] = []
         let entryDate = Date()
       
-        for hourOffset in 0 ..< 5 {
+        for hourOffset in 0 ..< 3 {
         lookAhead.append(Calendar.current.date(byAdding: .hour, value: hourOffset, to: entryDate)!)
         }
 
@@ -284,8 +313,11 @@ struct widget_Previews: PreviewProvider {
     }
   
     static var previews: some View {
-        let start = Calendar.current.date(byAdding: .minute, value: -120, to: Date())!
-        let end = Calendar.current.date(byAdding: .minute, value: 120, to: Date())!
+        let start = Calendar.current.date(byAdding: .minute, value: 1, to: Date())!
+        let end = Calendar.current.date(byAdding: .minute, value: 60, to: Date())!
+      
+      let start2 = Calendar.current.date(byAdding: .minute, value: 60, to: Date())!
+      let end2 = Calendar.current.date(byAdding: .minute, value: 120, to: Date())!
       
         widgetEntryView(entry: SimpleEntry(date: Date(),
            lookAhead: calculateLookAhead(),
@@ -294,9 +326,29 @@ struct widget_Previews: PreviewProvider {
             end: end,
             title: "test modul",
             status: Status.normal,
-            overlaps: 2,
-            _id: "unique id"
-        )]))
+            _id: "unique id",
+            
+            width: 0.5,
+            left: 0.0
+        ), Module(
+          start: start,
+          end: end,
+          title: "test modul",
+          status: Status.changed,
+          _id: "unique id 2",
+          
+          width: 0.5,
+          left: 0.5
+      ), Module(
+        start: start2,
+        end: end2,
+        title: "test modul",
+        status: Status.cancelled,
+        _id: "unique id 3",
+        
+        width: 1,
+        left: 0
+      )], error: true))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
