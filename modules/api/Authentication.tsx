aa5b64d901925cd303c774eb75a2be23caec30fd
Tandhjulet <madsbechmortensen@hotmail.dk>
2024-11-19 21:38:40 +0100

    removed unnessacry text

--- modules/api/Authentication.tsx
+++ modules/api/Authentication.tsx
@@ -63,17 +63,18 @@ export async function validate(gymNummer: string, username: string, password: st
         body: stringifiedData,
     });
 
-    let text = await res.text();
+	let req = res;
     if(res.url !== `https://www.lectio.dk/lectio/${gymNummer}/forside.aspx`) {
-        text = await (await fetch(SCRAPE_URLS(gymNummer).FORSIDE, {
+        req = await fetch(SCRAPE_URLS(gymNummer).FORSIDE, {
             method: "POST",
             credentials: "include",
             headers: {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
             },
             body: stringifiedData,
-        })).text();
+        });
     }
+	let text = await req.text();
     const isAuth = (res.url == SCRAPE_URLS(gymNummer).FORSIDE && text.includes("Log ud"));
 
     if(isAuth) {
